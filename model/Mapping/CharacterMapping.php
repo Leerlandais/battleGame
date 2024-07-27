e0c4ef40fe1308fba1bf2689f15453cba014ebf2
leerlandais <leerlandais@gmail.com>
2024-07-27 10:06:07 +0100

    character: added getters/setters

--- model/Mapping/CharacterMapping.php
+++ model/Mapping/CharacterMapping.php
@@ -19,6 +19,72 @@ protected ?int $bg_char_hp;
 protected ?int $bg_char_att;
 protected ?int $bg_char_def;
 
+    public function getBgCharId(): ?int
+    {
+        return $this->bg_char_id;
+    }
+
+    public function setBgCharId(?int $bg_char_id): void
+    {
+        if (!$this->verifyInt($bg_char_id)) throw new Exception('Invalid BG Character ID');
+        $this->bg_char_id = $bg_char_id;
+    }
+
+    public function getBgCharOwnerId(): ?int
+    {
+        return $this->bg_char_owner_id;
+    }
+
+    public function setBgCharOwnerId(?int $bg_char_owner_id): void
+    {
+        if (!$this->verifyInt($bg_char_owner_id)) throw new Exception('Invalid BG Character Owner ID');
+        $this->bg_char_owner_id = $bg_char_owner_id;
+    }
+
+    public function getBgCharExp(): ?int
+    {
+        return $this->bg_char_exp;
+    }
+
+    public function setBgCharExp(?int $bg_char_exp): void
+    {
+        if (!$this->verifyInt($bg_char_exp)) throw new Exception('Invalid BG Character Exp');
+        $this->bg_char_exp = $bg_char_exp;
+    }
+
+    public function getBgCharHp(): ?int
+    {
+        return $this->bg_char_hp;
+    }
+
+    public function setBgCharHp(?int $bg_char_hp): void
+    {
+        if(!$this->verifyInt($bg_char_hp)) throw new Exception('Invalid BG Character Hp');
+        $this->bg_char_hp = $bg_char_hp;
+    }
+
+    public function getBgCharAtt(): ?int
+    {
+        return $this->bg_char_att;
+    }
+
+    public function setBgCharAtt(?int $bg_char_att): void
+    {
+        if(!$this->verifyInt($bg_char_att)) throw new Exception('Invalid BG Character Att');
+        $this->bg_char_att = $bg_char_att;
+    }
+
+    public function getBgCharDef(): ?int
+    {
+        return $this->bg_char_def;
+    }
+
+    public function setBgCharDef(?int $bg_char_def): void
+    {
+        if(!$this->verifyInt($bg_char_def)) throw new Exception('Invalid BG Character Def');
+        $this->bg_char_def = $bg_char_def;
+    }
+
 
 
 } // end class
